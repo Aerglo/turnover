df069680c7a0e0f21a2e88916575d4388c7c958b
aerglo <imaerglo@gmail.com>
2023-07-01 10:54:27 +0330

    appbar color changed

--- lib/Presentation/Constants/constants.dart
+++ lib/Presentation/Constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class PresentationConstants {
   static Color backgroundColor = Colors.grey.shade800;
-  static Color appbarColor = const Color.fromARGB(255, 92, 161, 201);
+  static Color appbarColor = const Color.fromARGB(255, 236, 114, 114);
   static Color greenColor = const Color.fromARGB(255, 29, 194, 35);
   static Color redColor = const Color.fromARGB(255, 224, 63, 51);
   static Color greyColor = const Color.fromARGB(255, 178, 178, 178);
